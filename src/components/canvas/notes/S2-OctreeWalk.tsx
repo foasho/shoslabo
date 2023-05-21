36998438ba616c659317896c12325db87b8919ec
foasho <foa.sho0111@outlook.jp>
2023-05-21 19:32:50 +0900

    feat: S3-grass

---
+++ src/components/canvas/notes/S2-OctreeWalk.tsx
@@ -0,0 +1,12 @@
+
+
+export const MyScene = () => {
+  return (
+    <>
+      <mesh>
+        <boxBufferGeometry attach="geometry" />
+        <meshBasicMaterial attach="material" color="hotpink" />
+      </mesh>
+    </>
+  )
+}
