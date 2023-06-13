cfeb4dd534053a616fe981f480b7d026e3dfa9b5
foasho <foa.sho0111@outlook.jp>
2023-06-13 09:18:50 +0900

    update: home更新

--- src/components/canvas/Home.tsx
+++ src/components/canvas/Home.tsx
@@ -1,10 +1,11 @@
-import { Environment, MeshReflectorMaterial, OrbitControls, Scroll, ScrollControls, Sky, useGLTF, useScroll, useTexture } from "@react-three/drei"
-import { useThree, applyProps, useLoader, useFrame } from "@react-three/fiber"
-import { EffectComposer, Bloom, SSR, LUT, Outline } from "@react-three/postprocessing"
-import { createContext, useContext, useEffect, useLayoutEffect, useState, useMemo, useRef } from "react"
-import { Group, Mesh, Vector3 } from "three"
-import { LUTCubeLoader } from 'postprocessing'
-
+import { Environment, MeshReflectorMaterial, OrbitControls, Scroll, ScrollControls, Sky, useGLTF, useScroll, useTexture } from "@react-three/drei";
+import { useThree, applyProps, useLoader, useFrame } from "@react-three/fiber";
+import { EffectComposer, Bloom, SSR, LUT, Outline, Noise, DepthOfField } from "@react-three/postprocessing";
+import { createContext, useContext, useEffect, useLayoutEffect, useState, useMemo, useRef } from "react";
+import { Group, Mesh, Vector3, ShaderMaterial, Color, DoubleSide, MeshStandardMaterial } from "three";
+import { LUTCubeLoader } from 'postprocessing';
+// @ts-ignore
+import CustomShaderMaterial from "three-custom-shader-material";
 
 export const Home = () => {
 
@@ -15,9 +16,7 @@ export const Home = () => {
       >
         <Lighting />
         <MyEffect />
-        <Scroll>
-          <Table />
-        </Scroll>
+        <Table />
         <MyCamera />
       </ScrollControls>
     </MyProvider>
@@ -64,14 +63,19 @@ const MyCamera = (
 
   return (
     <>
-      {/* <OrbitControls /> */}
+      <OrbitControls />
     </>
   )
 }
 
 const MyEffect = () => {
-  const { mode } = useMyContext();
+
+  const { mode, hovered } = useMyContext();
   const [texture, setTexture] = useState(null);
+  const selected = hovered ? [hovered] : undefined;
+
+  const edgeColor = mode === "dark" ? "#ffffe0" : "#191970";
+  const edgeStrength = mode === "dark" ? 10 : 100;
   useEffect(() => {
     const loader = new LUTCubeLoader();
     loader.load("/luts/std.cube", (loadedTexture) => {
@@ -89,9 +93,15 @@ const MyEffect = () => {
   return (
     <>
       {/* <color attach="background" args={['#171720']} /> */}
-      <EffectComposer disableNormalPass>
+      <EffectComposer multisampling={0} disableNormalPass autoClear={false}>
+        <DepthOfField
+          focusDistance={0}
+          focalLength={0.02}
+          bokehScale={2}
+          height={480}
+        />
         {bloomEffect}
-        <SSR
+        {/* <SSR
           temporalResolve={true}
           STRETCH_MISSED_RAYS={true}
           USE_MRT={true}
@@ -116,12 +126,24 @@ const MyEffect = () => {
           maxDepth={1}
           thickness={10}
           ior={1.45}
-        />
+        /> */}
         {lutEffect}
+        <Outline
+          selection={selected}
+          selectionLayer={10}
+          edgeStrength={edgeStrength}
+          // @ts-ignore
+          hiddenEdgeColor={edgeColor}
+          // @ts-ignore
+          visibleEdgeColor={edgeColor}
+          blur={true}
+        />
+        <Noise opacity={0.02} />
       </EffectComposer>
       <Environment
         preset={mode === "dark" ? "night" : "sunset"}
-        background blur={0.6}
+        background
+        blur={0.6}
       >
       </Environment>
     </>
@@ -179,9 +201,6 @@ const Table = () => {
 const ComputerDesk = () => {
   const [marbleTex] = useTexture(['/textures/marble.jpg']);
   const { nodes } = useGLTF('/models/computer_desk.glb') as any;
-  // comp_desk_top_2のGeometryを取得
-  const topGeometry = nodes['comp_desk_top_2'].children[0].geometry;
-  console.log(topGeometry);
   return (
     <group
       position={[0, -1.47, 0]}
@@ -193,24 +212,7 @@ const ComputerDesk = () => {
       <primitive object={nodes['comp_desk_remote_1']} />
       {/** 天板 */}
       <mesh>
-        <primitive object={topGeometry} attach="geometry" />
-        <MeshReflectorMaterial
-          resolution={1024}
-          metalness={1}
-          roughness={0.5}
-          envMapIntensity={1}
-          distortion={1}
-          // depthToBlurRatioBias={0.65}
-          // @ts-ignore
-          map={marbleTex}
-          isMaterial={true}
-          // distortionMap={marbleTex}
-          mirror={0.6}
-          reflectorOffset={0.2}
-          // 木目の色
-          color={"#8F775F"}
-          transparent={true}
-        />
+        <primitive object={nodes['comp_desk_top_2']} />
       </mesh>
       {/** モニター */}
       <Monitor />
@@ -242,8 +244,17 @@ const Monitor = () => {
  * ノート
  */
 const NoteBook = () => {
+  const topRef = useRef<any>();
   const { nodes } = useGLTF('/models/notebook.glb') as any;
-  // console.log(nodes);
+  const scroll = useScroll();
+  const [tex] = useTexture(['/textures/industrial-robot.png']);
+
+  useFrame((state, delta) => {
+    if (!topRef.current) return;
+    const data = scroll.range(0, 0.2);
+    topRef.current.rotation.x = data * Math.PI;
+  });
+
   return (
     <group
       scale={0.2}
@@ -253,6 +264,13 @@ const NoteBook = () => {
       }
     >
       {/** Pages */}
+      <mesh
+        position={[0, 0.11, -0.95]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <planeGeometry args={[2.8, 1.95, 1, 1]} />
+        <NoteMaterial />
+      </mesh>
       <primitive object={nodes['Cube002_7']} />
       <primitive object={nodes['Cube003_8']} />
       <primitive object={nodes['Cube004_9']} />
@@ -271,7 +289,7 @@ const NoteBook = () => {
 
       {/** Cover */}
       <primitive object={nodes['cover_down_1']} />
-      <primitive object={nodes['cover_up_2']} />
+      <primitive object={nodes['cover_up_2']} ref={topRef} />
 
       {/** Rings */}
       <primitive object={nodes['notrbook_ring_6']} />
@@ -280,14 +298,51 @@ const NoteBook = () => {
 }
 
 const Pen = () => {
-  const { scene } = useGLTF('/models/pen.glb') as any;
+  const scroll = useScroll();
+  const penRef = useRef<any>();
+  const { nodes } = useGLTF('/models/pen.glb') as any;
+  useFrame((state, delta) => {
+    if (!penRef.current) return;
+    const data = scroll.range(0.0, 0.2);
+    const maxY = 3;
+    if (data > 0.00001) {
+      penRef.current.rotation.x = data * Math.PI / 2;
+      penRef.current.position.x = 7.5 * data;
+      penRef.current.position.y = maxY * data;
+    }
+    const write = scroll.range(0.2, 0.4);
+    if (write > 0.00001) {
+      const val = write * 100;
+      // penRef.current.rotation.x = Math.PI / 2 + Math.PI / 24 * Math.sin(val);
+      penRef.current.rotation.z = Math.PI / 12 * Math.cos(val);
+      // 位置を左上から右下に移動
+      penRef.current.position.x = 7.5 + - 5 * write;
+      penRef.current.position.z = -8 * write;
+      if (write > 0.8) {
+        // 初期値に戻す
+        // 0.8 ~ 1.0 で上に移動
+        penRef.current.position.x = 0;
+        penRef.current.position.z = 0;
+        // 0.8 ~ 1.0 で上に移動
+        penRef.current.position.y = maxY * (write - 0.8);
+        penRef.current.rotation.x = 0;
+        penRef.current.rotation.z = 0;
+      }
+    }
+
+  });
   return (
     <group
       rotation={[0, Math.PI, 0]}
       position={[0.3, 0.74, 0]}
       scale={0.03}
     >
-      <primitive object={scene} />
+      {/** 本体 */}
+      <mesh ref={penRef}>
+        <primitive object={nodes['Stylo1']} />
+      </mesh>
+      {/** キャップ */}
+      <primitive object={nodes['Capuchon1']} />
     </group>
   )
 }
@@ -295,7 +350,6 @@ const Pen = () => {
 const Alexa = () => {
   const ref = useRef<any>();
   const { mode, setMode, setHovered: onHover, hovered } = useMyContext();
-  const selected = hovered ? [hovered] : undefined;
   const { scene, nodes } = useGLTF('/models/alexa.glb') as any;
   const [nightmodeTex, lightmodeTex, arrow] = useTexture(['/textures/nightmode.png', '/textures/lightmode.png', '/textures/right-arrow.png']);
   scene.traverse(function (child) {
@@ -303,8 +357,6 @@ const Alexa = () => {
       child.layers.enable(10)
     }
   });
-  const edgeColor = mode === "dark" ? "#ffffe0" : "#191970";
-  const edgeStrength = mode === "dark" ? 10 : 100;
   return (
     <>
       <group
@@ -358,7 +410,7 @@ const Alexa = () => {
           </mesh>
         </group>
       }
-      <EffectComposer multisampling={8} autoClear={false}>
+      {/* <EffectComposer multisampling={8} autoClear={false}>
         <Outline
           selection={selected}
           selectionLayer={10}
@@ -369,7 +421,90 @@ const Alexa = () => {
           visibleEdgeColor={edgeColor}
           blur={true}
         />
-      </EffectComposer>
+      </EffectComposer> */}
     </>
   )
 }
+
+const NoteMaterial = (
+  {
+    textColor = '#000000',
+    texUrl = '/textures/robot_alpha.jpg',
+  }
+) => {
+  const scroll = useScroll();
+  const ref = useRef<any>(null);
+  const alphaMap = useTexture(texUrl);
+
+  useFrame((state, delta) => {
+    const value = scroll.range(0.2, 0.4);
+    if (ref.current) {
+      ref.current.uniforms.uAlpha.value = value;
+    }
+  });
+
+  const uniforms = useMemo(() => ({
+    uTex: { value: alphaMap },
+    uAlpha: { value: 0.01 },
+    uColor: { value: new Color(textColor) },
+  }), []);
+
+  const shaderMaterial = new ShaderMaterial({
+    uniforms,
+    vertexShader,
+    fragmentShader,
+    transparent: true,
+    // depthWrite: false,
+    // depthTest: false,
+    // blending: AdditiveBlending,
+  });
+
+  return (
+    // <CustomShaderMaterial
+    //   alphaMap={alphaMap}
+    //   // alphaTest={0.5}
+    //   // baseMaterial={MeshStandardMaterial}
+    //   // color={new Color(textColor)}
+    //   uniforms={uniforms}
+    //   ref={ref}
+    //   fragmentShader={fragmentShader}
+    // />
+    <>
+      <primitive
+        object={shaderMaterial}
+        attach="material"
+        ref={ref}
+      />
+    </>
+  )
+}
+
+/**
+ * Vertex
+ */
+const vertexShader = `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+/**
+ * Fragment
+ */
+const fragmentShader = `
+  varying vec2 vUv;
+  uniform sampler2D uTex;
+  uniform float uAlpha;
+  uniform vec3 uColor;
+
+  void main() {
+    vec4 tex = texture2D(uTex, vUv);
+    float alpha = tex.r * uAlpha;
+    gl_FragColor = vec4(uColor, alpha);
+
+  }
+
+`;
