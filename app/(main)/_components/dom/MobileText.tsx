df833a1a8527efb65de8a48e258bc98d9a073302
foasho <foa.sho0111@outlook.jp>
2023-10-29 16:34:30 +0900

    delete: start

--- app/(main)/_components/dom/MobileText.tsx
+++ app/(main)/_components/dom/MobileText.tsx
@@ -1,40 +1,22 @@
 "use client";
-import React, { useState } from "react";
-import Image from "next/image";
-import { motion, animate, MotionConfig } from "framer-motion";
-import { gsap } from "gsap";
-import { useTimeManager } from "../../_providers/TimeManeger";
+import { motion } from "framer-motion";
 import { Typography } from "@/components/commons/Typography";
 
 export const MobileText = () => {
-
-  const { start } = useTimeManager();
-
   return (
     <>
-      {start &&
-        <>
-          <Typography variant="h1" className="fixed left-0 top-24 z-20 block w-full select-none pl-24 sm:pl-48 md:hidden">
-            <span className="text-[#E6B422]">
-              金沢に住む
-            </span>
-          </Typography>
-          <Typography variant="h1" className="fixed left-0 top-36 z-20 block w-full select-none pl-32 sm:text-center md:hidden">
-            <motion.span
-              key={'it-text'}
-              whileHover={{ scale: 1.4 }}
-              whileTap={{ scale: 0.9 }}
-            >
-              <span className="text-[#E6B422]">
-                ITエンジニア
-              </span>
-              <span className="text-[16px] text-[#E6B422]">
-                です。
-              </span>
-            </motion.span>
-          </Typography>
-        </>
-      }
+      <Typography variant='h1' className='fixed left-0 top-24 z-20 block w-full select-none pl-24 sm:pl-48 md:hidden'>
+        <span className='text-[#E6B422]'>金沢に住む</span>
+      </Typography>
+      <Typography
+        variant='h1'
+        className='fixed left-0 top-36 z-20 block w-full select-none pl-32 sm:text-center md:hidden'
+      >
+        <motion.span key={"it-text"} whileHover={{ scale: 1.4 }} whileTap={{ scale: 0.9 }}>
+          <span className='text-[#E6B422]'>ITエンジニア</span>
+          <span className='text-[16px] text-[#E6B422]'>です。</span>
+        </motion.span>
+      </Typography>
     </>
   );
 };
